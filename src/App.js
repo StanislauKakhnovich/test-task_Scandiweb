ab79641a007beafcd7dd4d7379610f23bfdef208
Stanislau Kakhnovich <stas_2002@tut.by>
2022-07-04 10:15:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+
+class App extends React.Component {
+
+  
+  render() {
+    return (
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
